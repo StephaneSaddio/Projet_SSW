ee0f5a472eb8728ab727a226f82d1cba2f097709
Sophie Manuel <sophie.manuel@etu.umontpellier.fr>
2021-05-20 18:02:23 +0200

    ajout de la fonction de risque + plot dans la partie 7.

--- Code/dens_func.R
+++ Code/dens_func.R
@@ -1,8 +1,8 @@
 seque <- seq(0,30, length.out = 60)
-ychi2 <- dchisq(seque,6)
+ychi2 <- dchisq(seque, 6)
 plot(seque, ychi2, type = "l", col = "red")
 
-rand <-rnorm(100 ,15,1)
+rand <-rchisq(100, 6)
 hist(rand, breaks = 60, freq = F, xlim=c(0, max(rand)+3))
 
 #      hat_f <- function(x, X, bandwith, kern) { # X est l'échantillon, x de \hat f(x)
@@ -13,9 +13,9 @@ hist(rand, breaks = 60, freq = F, xlim=c(0, max(rand)+3))
 #        return(hatf)
 #      }
       
-dens <- function(X, bandwith, kernel){
+dens <- function(sample, bandwith, kernel){
 
-  
+      X <- sample
       n <- length(X)
       x <- seq.int(min(X), max(X), length.out = n)
       if (missing(kernel)) {kernel = "gaussian"}
@@ -35,7 +35,6 @@ dens <- function(X, bandwith, kernel){
     if (missing(bandwith)){
         
       bandwith <- seq(0,5,0.1)
-      bw
       s <- 0
       for (i in 1:n){ 
       s<- s + sum(kern( (X[i]-X[-i])/bandwith)) 
@@ -62,12 +61,71 @@ dens <- function(X, bandwith, kernel){
       
   
 }
+plot(rand)
+rand[rand=a$y]
 
-
-
-test = dens(rand, bandwith = "ucv", "rectangular")
-plot(seq(min(rand), max(rand), length.out =  1000), test)
-lines(dchisq(0:100,6)) 
-lines(density(rand, kernel = "rectangular", bw="ucv"), col = "red")
+test = dens(rand, bandwith = 1.1)
+plot(seq(min(rand), max(rand), length.out =  100), type="l", test)
+lines(dchisq(0:21, 6)) 
+lines(density(rand, kernel = "gaussian"), col = "red")
 a=density(rand)
 str(a)
+
+Risk <- function(sample, bandwith, kernel){
+  
+  X  <- sample
+  n <- length(X)
+  x <- seq.int(min(X), max(X), length.out = n)
+  if (missing(kernel)) {kernel = "gaussian"}
+  kern <- switch(kernel,
+                 gaussian = function(u){return(dnorm(u))},
+                 rectangular = function(u){
+                   ifelse(abs(u) < 1, .5, 0) },
+                 triangular = function(u) {
+                   return(ifelse(abs(u) < 1, (1 - abs(u)), 0)) },
+                 epanechnikov = function(u){
+                   return(ifelse(abs(u) < 1, 3/4*(1 - u^2), 0)) },
+                 biweight = function(u){ 
+                   return(ifelse(abs(u) < 1, 15/16*(1 - u^2)^2, 0)) }) # noyau caractère en fonction
+  
+  hatf <- function(x, sample, h, kernel){
+    n <- length(sample)
+    som <- 0
+    for (i in 1:n){
+     som <- som + kern((sample[i]-x)/h) 
+    }
+    return(som/(n*h))
+  } 
+   
+  s <- 0
+  for (i in 1:n){ 
+    s <- s + sum(kern( (X[i]-X[-i])/bandwith)) 
+  }
+  
+
+  n2hat_f <- integrate(function(x) hatf(x, sample = X, h = bandwith, kernel = kern), min(X)-50, max(X)+50)$value ^2
+
+  R <- n2hat_f - 2/(n*(n-1))* s /bandwith
+
+  return(R)
+}
+
+Risk(rand, bandwith = 0.025)
+
+x <- seq(0.03, 1, by = 0.01)
+risvec <- NULL
+j=1
+for (i in x){risvec[j]=Risk(rand,i); j=j+1}
+
+plot(x, risvec)
+
+
+
+
+
+MSE <- function(m, o){
+  mean((m - o)^2)
+}
+id <- function(x){x+1}
+id(2)
+ 
